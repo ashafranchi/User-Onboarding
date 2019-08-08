8c149334df62a6a1f09c1719cdc2142e6b38e4d7
Asha <asha17@brown.edu>
2019-08-08 10:26:43 -0700

    mvp met

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <FormikForm/>
+      <FormikForm />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { withFormik, Form, Field } from "formik";
-// import * as Yup from "yup";
-// import axios from "axios";
+import * as Yup from "yup";
 
-const userForm = ({errors, touched, values}) => {
+const UserForm = ({errors, touched, values, status}) => {
+    const [users, setUsers] = useState([])
+    console.log(users);
+
+    useEffect(() => {
+        if (status) {
+          setUsers([...users, status]);
+        }
+      }, [status]);
+    
     return(
-        <Form>
-            <Field type="text" name="username"/>
-            {touched.name && errors.name && <p>{errors.name}</p>}
-            <Field type="email" name="email"/>
-            {touched.email && errors.email && <p>{errors.email}</p>}
-            <Field type="password" name="password"/>
-            {touched.password && errors.password && <p>{errors.password}</p>}
-            <Field type="checkbox" name="terms" checked={values.terms}/>
-            <h3>Terms of Service</h3>
-            <button type="submit">Submit</button>
-        </Form>
+        <div className="format">
+            <Form>
+                <Field type="text" name="username"/>
+                {touched.name && errors.name && <p>{errors.name}</p>}
+                <Field type="email" name="email"/>
+                {touched.email && errors.email && <p>{errors.email}</p>}
+                <Field type="password" name="password"/>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                <Field type="checkbox" name="terms" checked={values.terms}/>
+                <h3>Terms of Service</h3>
+                <button type="submit">Submit</button>
+            </Form>
+            {users.map(user => (
+        <p key={user.id}>{user.username}</p>
+      ))}
+        </div>
     )
-}
+};
 
 const FormikForm = withFormik({
     mapPropsToValues({username, email, password, terms}) {
@@ -28,9 +42,17 @@ const FormikForm = withFormik({
             terms: terms || false
         };
     },
-    handleSubmit() {
-        
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required("Name!"),
+        email: Yup.string().required("Email!")
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        axios
+            .post("https://reqres.in/api/users_", values)
+            .then(res => {setStatus(res.data)})
+            .catch(err => console.log(err.response))
     }
-})(userForm);
+})(UserForm);
 
 export default FormikForm;
